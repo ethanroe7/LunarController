8064bdb3c1a9bfb4d167b359d9f72d4b5584a03c
ethanroe7 <ethan.roe@outlook.com>
2019-05-16 11:21:50 +0100

    Update Controller.c

--- Controller.c
+++ Controller.c
@@ -184,11 +184,9 @@ void getCondition(int fd, struct addrinfo *address) {
         condition = strtok(NULL, ":");
     }
    
-    char *landerFuelStr = strtok(conditions[2], "%");
-    char *landerAltitudeStr = strtok(conditions[3], "contact");
-
-    landerFuel = strtof(landerFuelStr, NULL);
-    landerAltitude = strtof(landerAltitudeStr, NULL);
+    char *landerFuel_ = strtok(conditions[2], "%");
+    landerFuel = landerFuel_;
+    landerAltitude = strtok(conditions[3], "contact");
 
     if(fuelBefore == -1) {
 	fuelBefore = landerFuel +1;
